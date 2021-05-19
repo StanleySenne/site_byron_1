f6813693fd51c63c0f3fe69aa4078fcd764143ae
StanleySenne <56733982+StanleySenne@users.noreply.github.com>
2021-05-18 20:07:08 -0800

    Add files via upload

---
+++ byron/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<title>byron.solutions</title>
+
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+SC' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/style.css">
+	</head>
+
+	<body>
+		
+		<header>
+			<nav>
+				<ul>
+					<li class="current_page_item"><a href="index.html">Home</a></li>
+					<li><a href="Portfolio.html">Portfólio</a></li>
+					<li><a href="Blog.html">Blog</a></li>
+					<li><a href="Sobre.html">Quem Somos</a></li>
+				</ul>
+			</nav>
+
+			<h1><img src="<?php echo get_stylesheet_directory_uri() ?>/img/logo_byron.png" alt="byron"></h1>
+		</header>
+	</body>
+</html>
